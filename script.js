d9645a91e12b67142c4b13b474cec89ffbbb6535
Ayush-Rajput123 <131739732+Ayush-Rajput123@users.noreply.github.com>
2025-07-23 17:23:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+const openModalBtn = document.getElementById('openModal');
+    const modal = document.getElementById('modal');
+    const closeModalBtn = document.querySelector('.close-modal');
+    openModalBtn.addEventListener('click', () => {
+      modal.style.display = 'block';
+    });
+    closeModalBtn.addEventListener('click', () => {
+      modal.style.display = 'none';
+    });
+    window.addEventListener('click', (event) => {
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
+
